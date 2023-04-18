5ae39f2c049eb08e4a40fe12457d39ea6980b14d
Francesco Re <francesco.re1107@gmail.com>
2023-04-18 15:04:46 +0200

    WIP receiving and parsing BLE packets in data_acquisition

--- data_acquisition/ble_test.py
+++ data_acquisition/ble_test.py
@@ -16,26 +16,39 @@ async def discover_bluetooth_device():
 def to_float(b):
     return struct.unpack('f', b)[0]
 
-all = set(range(1000))
-found = set()
+def to_int(b):
+    return struct.unpack('i', b)[0]
+
+def print_sample(sample):
+    print(f"{sample['id']:03d}", end=": ")
+    print(*sample.values()[:-1], sep=";")
+
+def callback(sender, data):
+    sample = []
+
+    # split data into chunks of 28 bytes
+    chunks = [data[i:i+28] for i in range(0, len(data), 28)]
+
+    for chunk in chunks:
+
+        # split chunk into 7 floats
+        vals = [to_float(chunk[i:i+4]) for i in range(0, len(chunk) - 4, 4)]
+        vals.append(to_int(chunk[-4:])) # last 4 bytes are the id
+        labels = ["accX", "accY", "accZ", "gyrX", "gyrY", "gyrZ", "id"]
+    
+        sample.append(dict(zip(labels, vals)))
+        print_sample(sample[-1])
 
 async def connect():
-    address = await discover_bluetooth_device()
+    address = DEVICE_ADDRESS #await discover_bluetooth_device()
     
-    async with BleakClient(address) as client:
+    client = BleakClient(address, disconnected_callback=lambda x: print("Disconnected"))
+    try:
+        await client.connect()
         print("Connected")
-        
-        while True:
-            accX = to_float(await client.read_gatt_char('19B10001-E8F2-537E-4F6C-D104768A1214'))
-            accY = to_float(await client.read_gatt_char('19B10002-E8F2-537E-4F6C-D104768A1214'))
-            accZ = to_float(await client.read_gatt_char('19B10003-E8F2-537E-4F6C-D104768A1214'))
-
-            gyrX = to_float(await client.read_gatt_char('19B1F001-E8F2-537E-4F6C-D104768A1214'))
-            gyrY = to_float(await client.read_gatt_char('19B1F002-E8F2-537E-4F6C-D104768A1214'))
-            gyrZ = to_float(await client.read_gatt_char('19B1F003-E8F2-537E-4F6C-D104768A1214'))
-
-            print(accX, accY, accZ, gyrX, gyrY, gyrZ)
-
+        await client.start_notify("19B10001-E8F2-537E-4F6C-D104768A1214", callback)
+    except Exception as e:
+        print(e)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(connect())
