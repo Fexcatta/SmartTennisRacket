e5f8cb5d5c82551c65cca580abae3739d19feaa8
Francesco Re <francesco.re1107@gmail.com>
2023-04-28 15:40:29 +0200

    Fix error message

--- data_acquisition/config.py
+++ data_acquisition/config.py
@@ -4,8 +4,8 @@ try:
     with open("config.yaml", "r") as stream:
         config = yaml.safe_load(stream)
 except yaml.YAMLError as exc:
-    print("Cannot parse yaml")
+    print("Cannot parse config.yaml")
     exit()
 except FileNotFoundError:
-    print("Cannot find yaml")
+    print("Cannot find config.yaml")
     exit()
